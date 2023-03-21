fd329cd56cb35955ffa948bebfa6f76533659366
natalia.yelistratova <natalia.yelistratova@gmail.com>
2023-03-21 22:04:53 +0300

    Update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,6 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse()
