a33576559d06b68f0419eae9324af8b98223f5ae
natalia.yelistratova <natalia.yelistratova@gmail.com>
2023-03-23 22:42:45 +0300

    gendiff updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export default (path1, path2) => {
-    console.log(path.resolve(`${path1}`))
+export default (path1, path2, format) => {
+    console.log('path - ', path.resolve(path1))
+    console.log('format - ', format)
 }
