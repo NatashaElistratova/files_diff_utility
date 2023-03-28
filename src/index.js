b48fe6c2995a66f7bae9ea88a6baba45437f7977
natalia.yelistratova <natalia.yelistratova@gmail.com>
2023-03-28 13:53:06 +0300

    Removed unused flag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash'; 
 
 export default (path1, path2, format) => {
-    console.log('path - ', path.resolve(path1))
-    console.log('format - ', format)
+    // console.log('path - ', path.resolve(path1))
+    // console.log('format - ', format)
+
+    console.log(_.isPlainObject({x: 1, y: {d: 2}}) )
 }
