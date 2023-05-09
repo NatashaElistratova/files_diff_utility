0ffb93817c0c8e24c3d9a95a587dcdbd890b3639
natalia.yelistratova <natalia.yelistratova@gmail.com>
2023-05-09 19:09:02 +0300

    Add curly braces

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const getFileExt = (filepath) => filepath.split('.').at(-1);
 
+const getIndent = (isRoot = true, spaceCount = 4) => ' '.repeat(isRoot ? spaceCount : spaceCount - 2);
+
 const plainFormatter = {
-  added: (node) => `+ ${node.key}: ${node.value}\n`,
-  deleted: (node) => `- ${node.key}: ${node.value}\n`,
-  notChanged: (node) => `  ${node.key}: ${node.value}\n`,
-  changed: (node) => `- ${node.key}: ${node.value1}\n+ ${node.key}: ${node.value2}\n`,
+  added: (node, isRoot, spaceCount) => `${getIndent(isRoot, spaceCount)}+ ${node.key}: ${node.value}\n`,
+  deleted: (node, isRoot, spaceCount) => `${getIndent(isRoot, spaceCount)}- ${node.key}: ${node.value}\n`,
+  notChanged: (node, isRoot, spaceCount) => `${getIndent(isRoot, spaceCount)}  ${node.key}: ${node.value}\n`,
+  changed: (node, isRoot, spaceCount) => `${getIndent(isRoot, spaceCount)}- ${node.key}: ${node.value1}\n${getIndent(isRoot, spaceCount)}+ ${node.key}: ${node.value2}\n`,
+  nested: (node, isRoot, spaceCount) => `${getIndent(isRoot, spaceCount)}  ${node.key}:  {\n${getIndent(spaceCount)}  ${node.value}\n${getIndent(spaceCount)}  }\n`
 };
 
 const generateTree = (obj1, obj2) => {
@@ -28,14 +31,25 @@ const generateTree = (obj1, obj2) => {
     if (!_.has(obj1, key) && _.has(obj2, key)) {
       return { key, value: obj2[key], type: 'added' };
     }
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+        const diffData = generateTree(obj1[key], obj2[key])
+        return { key, value: format(diffData, plainFormatter, false), type: 'nested' };
+    }
 
     return {
-      key, value1: obj1[key], value2: obj2[key], type: 'changed',
+        key, value1: obj1[key], value2: obj2[key], type: 'changed',
     };
   });
 };
 
-const format = (tree, formatter) => tree.reduce((acc, node) => acc + formatter[node.type](node), '');
+const format = (tree, formatter, isRoot) => {
+    const result = tree.reduce((acc, node) => {
+        return acc + formatter[node.type](node, isRoot, 2)
+    }, `${isRoot ? `{\n` : ''}`)
+
+    return `${result}}`
+}
+
 
 export default (path1, path2) => {
   const filepath1 = getFullPath(path1);
@@ -48,5 +62,5 @@ export default (path1, path2) => {
 
   const diffData = generateTree(data1, data2);
 
-  return format(diffData, plainFormatter);
+  return format(diffData, plainFormatter, true);
 };
