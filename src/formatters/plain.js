412ef041039720098b644b49b11b2d84e1350865
natalia.yelistratova <natalia.yelistratova@gmail.com>
2023-05-22 15:05:21 +0300

    Fix tests

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,24 +10,25 @@ const formatValue = (value) => {
 };
 
 const format = (diffData, parentKeyName = '') => {
-  const result = diffData.map((node) => {
+  const result = diffData.map((node, index) => {
     if (node.type === 'unchanged') return;
     const keyName = parentKeyName ? `${parentKeyName}.${node.key}` : node.key;
+    const newLineSymbol = index === diffData.length - 1 ? '' : '\n';
 
-    return plainFormatter[node.type](node, keyName);
+    return `${plainFormatter[node.type](node, keyName)}${newLineSymbol}`;
   });
 
   return result.join('');
 };
 
 const plainFormatter = {
-  added: (node, keyName) => `Property '${keyName}' was added with value: ${formatValue(node.value)}\n`,
-  deleted: (node, keyName) => `Property '${keyName}' was removed\n`,
+  added: (node, keyName) => `Property '${keyName}' was added with value: ${formatValue(node.value)}`,
+  deleted: (node, keyName) => `Property '${keyName}' was removed`,
   changed: (node, keyName) => {
     const value1 = formatValue(node.value1);
     const value2 = formatValue(node.value2);
 
-    return `Property '${keyName}' was updated. From ${value1} to ${value2}\n`;
+    return `Property '${keyName}' was updated. From ${value1} to ${value2}`;
   },
   nested: (node, parentKeyName) => format(node.children, parentKeyName),
 };
